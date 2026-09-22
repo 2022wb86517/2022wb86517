d834acb4d9e187133ed55b8f166b4eb48040a035
2022wb86517 <2022wb86517@wilp.bits-pilani.ac.in>
2026-09-23 04:21:52 +0530

    Modified Source Code

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -5,6 +5,6 @@ package com.bits.devops;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Devops Lab Modified");
     }
 }
